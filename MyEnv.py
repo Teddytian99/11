56410a571bdb69513c45407c03fbff4b26a5341f
Teddytian99 <136065047+Teddytian99@users.noreply.github.com>
2023-06-09 16:03:41 +0800

    Update MyEnv.py

--- MyEnv.py
+++ MyEnv.py
@@ -11,15 +11,13 @@ tf.random.set_seed(42)
 
 
 class MyEnv(Env):
-    """
-    动作空间和状态空间都是连续的，奖励为机器学习预测的熔深与板厚之比减去孔隙率
-    """
+
 
     def __init__(self):
         self.state = [[2.875, 2.25, 0.7, 212.5]]
         self.seed(42)
-        self.std = load('D:/Users/12171/anaconda3/envs/tf28/Lib/site-packages/gym/envs/my_env/std.joblib')
-        model = tf.keras.models.load_model('D:/Users/12171/anaconda3/envs/tf28/Lib/site-packages/gym/envs/my_env/model2.h5')
+        self.std = load('std.joblib')
+        model = tf.keras.models.load_model('model2.h5')
         self.model = model
         self.observation_space = spaces.Box(low=np.array([[1.75, 1, 0.2, 25]]), high=np.array([[4, 3.5, 1.2, 400]]), shape=(1, 4), dtype=np.float64)
         self.action_space = spaces.Box(low=np.array([[-1, -1, -1, -1]]), high=np.array([[1, 1, 1, 1]]), shape=(1, 4), dtype=np.float64)
@@ -44,7 +42,7 @@ class MyEnv(Env):
 
     def step(self, action):
         a = [[0.0045, 0.005, 0.002, 0.75]]
-        next_state = np.clip(self.state + np.multiply(a, action), self.observation_space.low, self.observation_space.high)  # 通过两个关键字查找状态转移表中的后继状态
+        next_state = np.clip(self.state + np.multiply(a, action), self.observation_space.low, self.observation_space.high)
         self.action = action
         reward = self.run(next_state, self.state)
         self.state = next_state
